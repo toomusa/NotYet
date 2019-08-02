17df7b7b80495fafa9028ca563fcec001f726271
unknown <cmzolczynski@gmail.com>
2019-08-02 11:07:26 -0700

    navbar component

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,9 +4,20 @@ import "./style.css";
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="/home">
+           Not Yet 
+        </a>
+        <a className="navbar-brand" href="/dashboard">
+          Dashboard 
+        </a>
+        <a className="navbar-brand" href="/profile">
+          Profile 
+        </a>
+        <a className="navbar-brand" href="/new">
+          + New Channel 
+        </a>
+      </nav>
     )
 }
 
