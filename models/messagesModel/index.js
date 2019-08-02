05606e85d4f4718b53b2c3e5fec850c6714f830a
Binyan Lin <obinyans@gmail.com>
2019-08-02 14:51:22 -0700

    messages schema added

--- models/messagesModel/index.js
+++ models/messagesModel/index.js
@@ -2,8 +2,35 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const MessagesSchema = new Schema({});
+const MessagesSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  channel: { 
+    type: Schema.Types.ObjectId,
+    ref: "Channel",
+    required: true
+  },
+  content: {            //maybe call it comment, but need to change on review.js if so
+    type: String
+  },
+  likes: {
+    type: Number
+  },
+  timeStamp: {
+    date: new Date(Date.now())
+  }
+});
 
 const Messages = mongoose.model("messages", MessagesSchema);
 
 module.exports = Messages;
+
+// - message ( 
+//   _id, author: ref user._id, 
+//   channel: ref channel._id, 
+//   content: String, 
+//   likes: Integer 
+//   )
