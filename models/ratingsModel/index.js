1136ebbaaeba9beb8893968ff69980aa3be2550b
Binyan Lin <obinyans@gmail.com>
2019-08-02 14:53:07 -0700

    ratings schema added

--- models/ratingsModel/index.js
+++ models/ratingsModel/index.js
@@ -2,8 +2,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const RatingsSchema = new Schema({});
+const RatingsSchema = new Schema({
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  media_title: { //called showOrMovie
+    type: String,
+    required: true
+  },
+  comment: { //previously called summary
+    type: String,
+  },
+  rating: { //1-10
+    required: true,
+    type: Number
+  },
+  channel: {
+    type: Schema.Types.ObjectId,
+    ref: "Channel"
+  },
+  timeStamp: {
+    date: new Date(Date.now())
+  }
+});
 
 const Ratings = mongoose.model("ratings", RatingsSchema);
 
 module.exports = Ratings;
+
+// - review ( 
+//   _id, 
+//   author: ref user._id, 
+//   showOrMovie, 
+//   summary, 
+//   rating, 
+//   channel._id 
+//   )
