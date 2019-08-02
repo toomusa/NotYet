315618a4ea305c12461723dcb1b5b5a021c27a27
unknown <cmzolczynski@gmail.com>
2019-08-02 12:26:02 -0700

    navbar css styling

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,11 +4,11 @@ import "./style.css";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/home">
+        <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand text-bold" id="appname" href="/home">
            Not Yet 
         </a>
-        <a className="navbar-brand" href="/dashboard">
+        <a className="navbar-brand offset-md-8" href="/dashboard">
           Dashboard 
         </a>
         <a className="navbar-brand" href="/profile">
