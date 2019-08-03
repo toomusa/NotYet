c279d48ade2002462f06a3aa5aba7904e13a7e43
unknown <cmzolczynski@gmail.com>
2019-08-02 18:47:52 -0700

    adding routes

--- routes/apiRoutes/dbRoutes/index.js
+++ routes/apiRoutes/dbRoutes/index.js
@@ -5,10 +5,21 @@ const dbController = require("../../../controllers/dbController")
 const passportService = require("../../../services/passport");
 const authMiddlewares = require("../../../middlewares/authMiddlewares");
 
-router.route("")
-    .post();
+router.route("/saveShow")
+    .post(dbController.saveShow);
+router.route("/saveMovie")
+    .post(dbController.saveMovie);
 
-router.route("")
-    .post()
+router.route("/addChannel")
+    .post(dbController.addChannel);
+router.route("/inviteToChannel")
+    .post(dbController.inviteToChannel);
+router.route("/recieveInvite")
+    .post(dbController.recieveInvite);    
+
+router.route("/sendMessage")
+    .post(dbController.sendMessage);
+router.route("/deleteMessage")
+    .post(dbController.deleteMessage);
 
 module.exports = router;
