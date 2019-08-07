9976fa8d9c86de1b9da67b807b21073629e2fc79
Binyan Lin <obinyans@gmail.com>
2019-08-07 00:29:25 -0700

    created movieCompile function for populating mongoDB

--- controllers/apiController/index.js
+++ controllers/apiController/index.js
@@ -1,28 +1,65 @@
-const db     = require("../../models");
 const config = require("../../config");
+
 var mongoose = require("mongoose");
-// const router = express.Router();
-// const express = require('express');
-// var express = require('express');
-// var app = express();
-// var router = express.Router();
+const db = require("../../models");
+
+const Moment = require("moment");
+const axios = require("axios");
+const imageRoot = "https://image.tmdb.org/t/p/w500";
+
+mongoose.connect("mongodb://localhost/chatdb", { useNewUrlParser: true });
+
+//DO not call unless you are prepared
+const movieCompiler = async () => {
+  let page = 1;
+  let maxPage = 1000;
+  let release_date_lower = 2010; //changeable when data is ready
+  let release_date_current = Moment(Date.now()).format("YYYY/MM/DD");
+
+  for (page; page<maxPage; page++) {
+
+    let movieURL = `https://api.themoviedb.org/3/discover/movie?api_key=dadbb348c7503427d5277348f42629dc&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=${page}&release_date.gte=${release_date_lower}&release_date.lte=${release_date_current}`
+
+    let movieData = await axios.get(movieURL);
+      
+        // console.log(res.data);
+        maxPage = movieData.data.total_pages
+        // console.log(maxPage);
+        
+        let pageArray = movieData.data.results;
+
+        // for (let i = 0; i < pageArray.length; i++) {
+        let mongooseArray = await pageArray.map(page => {
+          let mongooseObj = {}
+          // movie_id
+          mongooseObj.movie_id = page.id
+          // title
+          mongooseObj.title = page.title
+          // poster_path
+          mongooseObj.poster_path = page.poster_path ? imageRoot + page.poster_path : undefined
+          // backdrop_path
+          mongooseObj.backdrop_path = page.backdrop_path ? imageRoot + page.backdrop_path : undefined
+          // release_date
+          mongooseObj.release_date = page.release_date
+          // vote_average
+          mongooseObj.vote_average = page.vote_average
+          // overview
+          mongooseObj.overview = page.overview
+          // runtime
+          // revenue
+          // tagline
+          // genre
+          // homepage
+          // console.log(mongooseObj);
+          return mongooseObj
+        })
+        console.log(mongooseArray);
+        db.Movie.create(mongooseArray);
+    }
+}
+
+
+// showCompiler();
+// movieCompiler();
 
 module.exports = {};
-
-
-//a user can:
-
-// - create channels
-// - view one chat (channel) at a time
-// - be invited to a channel
-// - invite others to any channel they are active(?) in
-// - write messages
-// - search a list of movies and show  
-// - can save a show or movie as a favorites
-// - can create a channel based on search results 
-// - write a review
-//   - no reviews no spoilers
-// - rate an episode when you mark watched 
-// - mute channel?
-// - unmute channel?
-// - leave a chat and see old history in archive
