501b390e98fa8c00b6c4eafad442a0cd51fd2f0c
Binyan Lin <obinyans@gmail.com>
2019-08-07 22:44:03 -0700

    blreactifychat

--- client/src/containers/Channels/index.js
+++ client/src/containers/Channels/index.js
@@ -1,15 +1,74 @@
 import React, {Component} from "react";
 import "./style.css";
-import Channel from "../../components/Channel"
-
+import {ProfileButton, AddButton, Channel} from "../../components/Channel"
 class Channels extends Component {
-  store = {
-
+  state = {
+    channelObj:[
+      {
+        date: "",
+        admin: "",
+        muted: "",
+        public: "",
+        starred: "",
+        ref_movie: "",
+        active_channels: "",
+        poster_path: "http://www.iconarchive.com/download/i60042/mattahan/ultrabuuf/Comics-Spiderman-Morales.ico"
+      },
+      {
+        date: "",
+        admin: "",
+        muted: "",
+        public: "",
+        starred: "",
+        ref_movie: "",
+        active_channels: "",
+        poster_path: "http://forreadingaddicts.co.uk/wp-content/uploads/2016/10/stranger-things-icon.jpg"
+      },
+      {
+        date: "",
+        admin: "",
+        muted: "",
+        public: "",
+        starred: "",
+        ref_movie: "",
+        active_channels: "",
+        poster_path: "https://pbs.twimg.com/profile_images/841697425060425729/CJcwG11O_400x400.jpg"
+      },
+      {
+        date: "",
+        admin: "",
+        muted: "",
+        public: "",
+        starred: "",
+        ref_movie: "",
+        active_channels: "",
+        poster_path: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/11769bda-90b1-4261-a076-a98b26ca0c91/db14ouy-bf5282f6-c3ef-4aba-8faa-d1a0b7ce2be2.png"
+      },
+      {
+        date: "",
+        admin: "",
+        muted: "",
+        public: "",
+        starred: "",
+        ref_movie: "",
+        active_channels: "",
+        poster_path: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/e54db164-1718-49f1-9fee-350d097dc176/daa6jku-8db8d96e-d2f9-4078-9953-605548c73724.png"
+      },
+    ]
   }
 
   render() {
     return (
-      <Channel/>
+      <div id="sidebar" className="dragscroll">
+        <ul className="sidebar-menu">
+          <ProfileButton/>
+          <AddButton/>
+          {this.state.channelObj.map( channel => (
+            <Channel date={channel.date} admin={channel.admin} muted={channel.muted} public={channel.public} starred={channel.starred} ref_movie={channel.ref_movie} active_channels={channel.active_channels} poster_path={channel.poster_path}/>
+          ))}
+          
+        </ul>
+      </div >
     )
   }
 };
